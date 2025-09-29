92e849714b114d080b835d36fea018e08b7013e3
RezColen3 <rhythmupa@gmail.com>
2025-09-29 06:17:47 +0530

    time to test in prod :)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,40 @@
 <script>
+	import { onMount } from 'svelte';
 	import EditList from './EditList.svelte';
 	import Nav from './nav.svelte';
 	import { currentEditingList, Lists } from './store';
 	import ViewLists from './viewLists.svelte';
 
 	$inspect($Lists);
+
+	onMount(() => {
+		Lists.set(
+			JSON.parse(localStorage.getItem('lists')) || [
+				{
+					name: 'Example Grocery Bill',
+					dateCreated: new Date().toLocaleDateString(),
+					items: [
+						{
+							name: 'Bananas',
+							costPerItem: 10,
+							Quantity: 12,
+							discountPercent: 10
+						},
+						{
+							name: 'Milk',
+							costPerItem: 60,
+							Quantity: 2,
+							discountPercent: 30
+						}
+					]
+				}
+			]
+		);
+
+		Lists.subscribe((value) => {
+			localStorage.setItem('lists', JSON.stringify(value));
+		});
+	});
 </script>
 
 <div class="w-100 flex h-full flex-col gap-4 bg-white p-2">
--- src/routes/store.js
+++ src/routes/store.js
@@ -3,22 +3,21 @@ import { writable } from 'svelte/store';
 export var currentEditingList = writable(null);
 export var Lists = writable([
 	{
-		name: 'Example Grocery',
-		dateCreated: '',
-        items: [
-            {
-                name: "Bananas",
-                costPerItem: 10,
-                Quantity: 12,
-                discountPercent: 10
-            },
-            {
-                name: "Milk",
-                costPerItem: 60,
-                Quantity: 2,
-                discountPercent: 30
-            },
-        ]
+		name: 'Example Grocery Bill',
+		dateCreated: new Date().toLocaleDateString(),
+		items: [
+			{
+				name: 'Bananas',
+				costPerItem: 10,
+				Quantity: 12,
+				discountPercent: 10
+			},
+			{
+				name: 'Milk',
+				costPerItem: 60,
+				Quantity: 2,
+				discountPercent: 30
+			}
+		]
 	}
 ]);
-
